79fb88478c63fab0cf23b30e23e40b008542208a
Shlomi Fish <shlomif@iglu.org.il>
2011-08-04 10:14:12 +0300

    Removed some meaningless tests.
    They were a leftover of the QUnit example.

--- abc-path/js/abc-path-test.js
+++ abc-path/js/abc-path-test.js
@@ -1,31 +1,6 @@
 "use strict";
 function test_abc_path()
 {
-
-    test("a basic test example", function() {
-        ok( true, "this test is fine" );
-        var value = "hello";
-        equals( "hello", value, "We expect value to be hello" );
-    });
-
-    module("Module A");
-
-    test("first test within module", function() {
-        ok( true, "all pass" );
-    });
-
-    test("second test within module", function() {
-        ok( true, "all pass" );
-    });
-
-    module("Module B");
-
-    test("some other test", function() {
-        expect(2);
-        equals( true, true, "failing test" );
-        equals( true, true, "passing test" );
-    });
-
     module("Constants");
 
     test("Constants Test", function() {
