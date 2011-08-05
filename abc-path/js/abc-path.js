70e531b028666178327bcb2d107c388797af24c4
Shlomi Fish <shlomif@iglu.org.il>
2011-08-05 23:52:35 +0300

    Implement MicrosoftRand shuffle.
    There does not appear to be an easy way to swap two elements in JS:
    
    http://stackoverflow.com/questions/872310/javascript-swap-array-elements .

--- abc-path/js/abc-path.js
+++ abc-path/js/abc-path.js
@@ -721,5 +721,20 @@ Class('ABC_Path.MicrosoftRand', {
             this.setSeed((this.getSeed() * 214013 + 2531011) & 0x7FFFFFFF);
             return ((this.getSeed() >> 16) & 0x7fff);
         },
+        max_rand: function(mymax) {
+            return this.rand() % mymax;
+        },
+        shuffle: function(deck) {
+            if (deck.length) {
+                var i = deck.length;
+                while (--i) {
+                    var j = this.max_rand(i+1);
+                    var tmp = deck[i];
+                    deck[i] = deck[j];
+                    deck[j] = tmp;
+                }
+            }
+            return deck;
+        },
     },
 });
