f9875adcaec734e6c17c2575558abb321bd16cf8
Shlomi Fish <shlomif@iglu.org.il>
2012-02-23 00:02:34 +0200

    Extract a method.

--- abc-path/Games-ABC_Path-Solver/lib/Games/ABC_Path/Solver/Board.pm
+++ abc-path/Games-ABC_Path-Solver/lib/Games/ABC_Path/Solver/Board.pm
@@ -304,7 +304,7 @@ sub _set_conclusive_verdict_for_letter
 
             $self->_set_verdict($letter, $xy,
                 # TODO : extract a method for coords equality
-                ((($l_xy->x == $xy->x) && ($l_xy->y == $xy->y))
+                ($l_xy->_equal($xy)
                     ? $ABCP_VERDICT_YES
                     : $ABCP_VERDICT_NO
                 )
