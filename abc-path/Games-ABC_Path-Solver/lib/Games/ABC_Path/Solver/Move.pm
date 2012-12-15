04a5434c7b1d248742677e074103cf08ad031a29
Shlomi Fish <shlomif@shlomifish.org>
2012-12-15 18:49:43 +0200

    Remove trailing space.

--- abc-path/Games-ABC_Path-Solver/lib/Games/ABC_Path/Solver/Move.pm
+++ abc-path/Games-ABC_Path-Solver/lib/Games/ABC_Path/Solver/Move.pm
@@ -95,8 +95,8 @@ sub bump
     my ($self) = @_;
 
     return ref($self)->new(
-        { 
-            text => $self->get_text(), 
+        {
+            text => $self->get_text(),
             depth => ($self->get_depth+1),
             vars => { %{$self->{_vars}}, },
         }
@@ -105,7 +105,7 @@ sub bump
 
 =head2 $self->get_var($name)
 
-This method returns the raw, unformatted value of the move's variable (or its 
+This method returns the raw, unformatted value of the move's variable (or its
 parameter) called $name. Each move class contains several parameters that can
 be accessed programatically.
 
