3f527077771f6157cdc69a19a35f14c9c999b1af
Shlomi Fish <shlomif@shlomifish.org>
2019-07-13 18:09:11 +0300

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- abc-path/js/benchmark.ts
+++ abc-path/js/benchmark.ts
@@ -1,7 +1,5 @@
 "use strict";
-import { MSRand } from "./ms-rand";
-import { FinalLayoutObj , Generator } from "./abc-path";
-import { Base, Board, Constants, shlomif_repeat } from "./abc-path-solver";
+import { Generator } from "./abc-path";
 for (let i=1; i <= 100; ++i) {
     const gen = new Generator({ seed: i });
     const riddle = gen.calc_riddle();
