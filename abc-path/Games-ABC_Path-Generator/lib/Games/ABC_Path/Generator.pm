ce8daaf6703530308778dc5732c9687209c6bcc8
Shlomi Fish <shlomif@shlomifish.org>
2020-10-15 18:41:29 +0300

    remove annocpan link

--- abc-path/Games-ABC_Path-Generator/lib/Games/ABC_Path/Generator.pm
+++ abc-path/Games-ABC_Path-Generator/lib/Games/ABC_Path/Generator.pm
@@ -373,42 +373,6 @@ DFS:
 
 Shlomi Fish, L<http://www.shlomifish.org/> .
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-games-abc_path-generator at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Games-ABC_Path-Generator>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Games::ABC_Path::Generator
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Games-ABC_Path-Generator>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Games-ABC_Path-Generator>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Games-ABC_Path-Generator>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Games-ABC_Path-Generator/>
-
-=back
 
 =head1 SEE ALSO
 
--- abc-path/Games-ABC_Path-Generator/lib/Games/ABC_Path/MicrosoftRand.pm
+++ abc-path/Games-ABC_Path-Generator/lib/Games/ABC_Path/MicrosoftRand.pm
@@ -101,44 +101,6 @@ it. This is using the fisher-yates shuffle.
 
 Shlomi Fish, L<http://www.shlomifish.org/> .
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-games-abc_path-generator at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Games-ABC_Path-Generator>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Games::ABC_Path::MicrosoftRand
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Games-ABC_Path-Generator>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Games-ABC_Path-Generator>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Games-ABC_Path-Generator>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Games-ABC_Path-Generator/>
-
-=back
-
 =cut
 
 1;    # End of Games::ABC_Path::MicrosoftRand
