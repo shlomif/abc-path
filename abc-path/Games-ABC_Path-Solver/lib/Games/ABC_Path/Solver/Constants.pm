2d12619fcbe4abb054e63d34ca157f2d1212359b
Shlomi Fish <shlomif@shlomifish.org>
2020-10-15 18:29:07 +0300

    remove annocpan link

--- abc-path/Games-ABC_Path-Solver/lib/Games/ABC_Path/Solver/Constants.pm
+++ abc-path/Games-ABC_Path-Solver/lib/Games/ABC_Path/Solver/Constants.pm
@@ -31,43 +31,6 @@ our @EXPORT =
 
 Shlomi Fish, L<http://www.shlomifish.org/> .
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-games-abc_path-generator at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Games-ABC_Path-Solver>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Games::ABC_Path::Solver::Constants
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Games-ABC_Path-Solver>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Games-ABC_Path-Solver>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Games-ABC_Path-Solver>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Games-ABC_Path-Solver/>
-
-=back
-
 =cut
 
 1;
--- abc-path/Games-ABC_Path-Solver/lib/Games/ABC_Path/Solver/Move/LettersNotInVicinity.pm
+++ abc-path/Games-ABC_Path-Solver/lib/Games/ABC_Path/Solver/Move/LettersNotInVicinity.pm
@@ -43,41 +43,6 @@ sub _format
 
 Shlomi Fish, C<< <shlomif at cpan.org> >>
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-games-abc_path-solver at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Games-ABC_Path-Solver>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Games::ABC_Path::Solver::Move
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Games-ABC_Path-Solver>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Games-ABC_Path-Solver>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Games-ABC_Path-Solver>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Games-ABC_Path-Solver/>
-
-=back
-
 =cut
 
 1;
