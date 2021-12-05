0a63af1fd1da5af8ae921c39356dbdb352034332
Shlomi Fish <shlomif@shlomifish.org>
2021-12-05 08:46:06 +0200

    add script

---
+++ abc-path/Games-ABC_Path-Generator/scripts/tag-release.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Path::Tiny qw/ path /;
+
+my ($version) =
+    ( map { m{\Aversion * = *(\S+)} ? ($1) : () }
+        path("./dist.ini")->lines_utf8() );
+
+if ( !defined($version) )
+{
+    die "Version is undefined!";
+}
+
+my @cmd = (
+    "git", "tag", "-m",
+    "Tagging Games-ABC_Path-Generator as $version",
+    "Games-ABC_Path-Generator-$version",
+);
+
+print join( " ", map { /\s/ ? qq{"$_"} : $_ } @cmd ), "\n";
+exec(@cmd);
