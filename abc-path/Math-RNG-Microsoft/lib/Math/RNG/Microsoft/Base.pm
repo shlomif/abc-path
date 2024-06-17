4c903ab0ad21cd8c8b6001c0b0c2a97bae289314
Shlomi Fish <shlomif@shlomifish.org>
2024-06-17 18:36:14 +0300

    add tests; update docs

--- abc-path/Math-RNG-Microsoft/lib/Math/RNG/Microsoft/Base.pm
+++ abc-path/Math-RNG-Microsoft/lib/Math/RNG/Microsoft/Base.pm
@@ -6,37 +6,21 @@ use warnings;
 
 =head1 NAME
 
-Math::RNG::Microsoft - a pseudo-random number generator compatible
-with Visual C.
+Math::RNG::Microsoft::Base - base class
 
 =head1 SYNOPSIS
 
-    use Math::RNG::Microsoft;
+    use Math::RNG::Microsoft::Base ();
 
-    my $randomizer = Math::RNG::Microsoft->new(seed => 24);
-
-    my $random_digit = $randomizer->rand_max(10);
+    [For internal use.]
 
 =head1 DESCRIPTION
 
-This is a random number generator used by L<Games::ABC_Path::Generator>, which
-emulates the one found in Microsoft's Visual C++. It was utilised here, out
-of familiarity and accessibility, because it is commonly used to generate
-Freecell layouts in the Freecell world (see
-L<http://en.wikipedia.org/wiki/FreeCell_%28Windows%29> ).
-
-B<NOTE:> This is not a cryptologically secure random number generator,
-nor is it a particularly good one, so its use is discouraged unless
-compatibility with the Windows C Run-time-library is needed.
+For internal use.
 
 =cut
 
 use integer;
-
-use Class::XSAccessor {
-    constructor => 'new',
-    accessors   => [qw(seed)],
-};
 
 sub fresh_shuffle
 {
@@ -77,22 +61,6 @@ sub shuffle
 }
 
 =head1 SUBROUTINES/METHODS
-
-=head2 new
-
-The constructor. Accepts a numeric seed as an argument.
-
-    my $randomizer = Math::RNG::Microsoft->new(seed => 1);
-
-=head2 $randomizer->rand()
-
-Returns a random integer from 0 up to 0x7fff - 1.
-
-    my $n = $randomizer->rand()
-
-=head2 $randomizer->seed($seed)
-
-Can be used to re-assign the seed of the randomizer (though not recommended).
 
 =head2 my $array_ref = $randomizer->shuffle(\@array)
 
