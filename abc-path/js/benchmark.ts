9f54b0a320f9017e029d3ad1e1c1a7c16e0aecff
Shlomi Fish <shlomif@shlomifish.org>
2024-06-27 09:14:16 +0300

    add support for selecting either --mt or msrand in benchmark.js
    "MT" is the mersenne-twister randomiser

--- abc-path/js/benchmark.ts
+++ abc-path/js/benchmark.ts
@@ -1,12 +1,21 @@
 "use strict";
-import * as MersenneTwister from "mersenne-twister" ;
+import * as MersenneTwister from "mersenne-twister";
 import { Generator } from "./abc-path";
-for (let i=1; i <= 100; ++i) {
-    const r = new MersenneTwister(i);
-    const max_rand = (m) => {return r.random_int() % m;};
-    const gen = new Generator({ max_rand: max_rand, });
-    const riddle = gen.calc_riddle();
 
+const is_mt: boolean = process.argv.length > 2 && process.argv[2] == "--mt";
+
+for (let i = 1; i <= 100; ++i) {
+    let gen;
+    if (is_mt) {
+        const r = new MersenneTwister(i);
+        const max_rand = (m) => {
+            return r.random_int() % m;
+        };
+        gen = new Generator({ max_rand: max_rand });
+    } else {
+        gen = new Generator({ seed: i });
+    }
+    const riddle = gen.calc_riddle();
     console.log("ABC Path Solver Layout Version 1:");
     console.log(riddle.get_riddle_v1_string());
 }
